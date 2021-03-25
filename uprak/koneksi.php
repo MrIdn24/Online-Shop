0f6d6f0d68727a126f62b307e7dec0870db1b63b
MrIdn24 <68394570+MrIdn24@users.noreply.github.com>
2021-03-25 17:14:31 +0700

    Delete uprak directory

--- uprak/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host       = "localhost";
-$user       = "root";
-$password   = "";
-$database   = "uprak";
-$conn   = mysqli_connect($host, $user, $password, $database) or die("Gagal Menghubungkan");
